2f423fe3eec1fd855e945ec2c316700d6accf989
Alexandre Karpov <alexakarpov@gmail.com>
2018-01-25 02:41:40 -0500

    timer using core async

--- src/coras/core.clj
+++ src/coras/core.clj
@@ -1,7 +1,16 @@
 (ns coras.core
+  (:require [clojure.core.async :as as])
   (:gen-class))
+
+(defn run-timer [n]
+  (as/go-loop [ticks 1]
+    (do
+      (println "tick..")
+      (Thread/sleep 1000)
+      (if (= ticks n) (println "bye!") (recur (+ 1 ticks))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello core.async")
+)

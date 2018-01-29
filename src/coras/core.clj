929829b29e8d2a52884d9ad94dfd506f439d74af
Alexandre Karpov <alexakarpov@gmail.com>
2018-01-28 21:42:31 -0500

    interactive session support

--- src/coras/core.clj
+++ src/coras/core.clj
@@ -1,8 +1,20 @@
 (ns coras.core
-  (:require [clojure.core.async :as a])
+  (:require [clojure.core.async :refer [chan >!!]])
   (:require [clojure.data.json :as json])
   (:require [coras.driver :as driver])
   (:gen-class))
+
+(def in-ch (atom nil))
+
+(defn open-interactive-channel []
+  (println "Initializing a channel (size 10) for an interactive session")
+  (reset! in-ch (chan 10)))
+
+(defn add-to-in-ch [event]
+  (let [size (.count (.buf @in-ch))]
+    (if (>= size 10 )
+      [:error :channel_size]
+      (>!! @in-ch event))))
 
 (def stdin-reader
   (java.io.BufferedReader. *in*))
@@ -10,8 +22,8 @@
 (defn -main
   [& args]
   (println "Creating an input channel from STDIN")
-  (let [in-chan (a/chan 10)]
+  (let [in-chan (chan 10)]
     (driver/run in-chan)
     (doseq [line (line-seq stdin-reader)]
       (println "read: " line)
-      (a/>!! in-chan line))))
+      (>!! in-chan line))))

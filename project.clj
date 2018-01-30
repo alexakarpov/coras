e3ed7716c6bcae1e95f4d49a83cf2eaa2a6797cf
Alexandre Karpov <alexakarpov@gmail.com>
2018-01-29 23:06:26 -0500

    add core.match dep

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.14.2"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  [org.clojure/core.async "0.4.474"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-time "0.14.2"]]
   :main ^:skip-aot coras.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/coras/events_test.clj
+++ test/coras/events_test.clj
@@ -3,9 +3,10 @@
    (clj-time [core :as t]
              [format :as f])
    [clojure.test :refer :all]
+   [clojure.core.match :refer [match]]
    [coras.events :refer :all]))
 
-(defonce time-regex #"(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})Z")
+(def time-regex #"(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})Z")
 
 (deftest event-generation-with-type-wo-ts
   (testing "event created with passed type, without a timestamp"
@@ -18,15 +19,22 @@
   (testing "event created without a passed type, with a timestamp"
     (let [ts (t/now)
           ft (f/unparse (f/formatters :date-time-no-ms) ts)
-          event (make-event "M1" :timestamp ts)]
-      (is (and (= (:type event) "CycleComplete")
-               (= (:machine_id event) "M1")
-               (= (:timestamp event) ft))))))
+          event (make-event "M1" :timestamp ts)
+          _ (println event)]
+      (is (match event
+                 {:type "CycleComplete"
+                  :timestamp ts
+                  :machine_id "M1"} true
+                 _ false)))))
 
-(deftest event-from-line-test
-  (testing "event is created from a line"
-    (let [event_line "Machine2 CycleComplete"
-          event (event-from-line event_line)]
-      (is (and (= (:type event) "CycleComplete")
-               (= (:machine_id event) "Machine2")
-               (re-matches time-regex (:timestamp event)))))))
+(deftest process-cc-test
+  (testing "CycleComplete event processed"
+    (let [src-event (make-event "M2")
+          ts (:timestamp src-event)
+          dest-event (process-event src-event)]
+      (is (match dest-event
+                 {:machine_id "M2"
+                  :timestamp ts
+                  :recorded_at _
+                  :type "MachineCycled"} true
+                 _ false)))))

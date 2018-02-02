23156c228832cf0d903f79731b1597845ad223ef
Alexandre Karpov <alexakarpov@gmail.com>
2018-02-02 18:16:22 -0500

    nake sure uberrjar task doesn't mess up the prod profile

--- project.clj
+++ project.clj
@@ -4,14 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.clojure/core.async "0.4.474"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-kafka "0.3.4"]
                  [yogthos/config "1.1"]
                  [clj-time "0.14.2"]]
-  :profiles {:prod {:resource-paths ["config/prod"]}
-             :dev  {:resource-paths ["config/dev"]}}
+  :plugins [[lein-pprint "1.2.0"]]
+  :profiles {:prod {:resource-paths ^:replace ["config/prod"]
+                    :plugins [[cider/cider-nrepl "0.16.0"]]}
+             :dev  {:resource-paths ["config/dev"]
+                    :plugins [[cider/cider-nrepl "0.16.0"]]}
+             :uberjar {:aot :all}}
   :main ^:skip-aot coras.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")

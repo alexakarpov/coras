7b6ef0da210deb4b3649bde450bc7cf34e3545b0
Alexandre Karpov <alexakarpov@gmail.com>
2018-06-13 19:57:05 -0400

    formatting

--- src/eplk/utils.clj
+++ src/eplk/utils.clj
@@ -14,8 +14,7 @@
   "Produces an infinite lazy sequence of events, starting at 'from', each 40 seconds from the previous one"
   (let [forty-seconds-later
         (fn [cnt]
-          (t/plus from (t/seconds (* cnt 40))))
-        ]
+          (t/plus from (t/seconds (* cnt 40))))]
     (for [i (iterate #(inc %) 0)]
       (e/make-event machine-id :timestamp (forty-seconds-later i)))))
 

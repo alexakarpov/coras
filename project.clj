e8b4ddbffe086bbea98f3544443086d079dcddaf
Alexandre Karpov <alexakarpov@gmail.com>
2018-06-15 00:47:36 -0400

    REST API in

--- project.clj
+++ project.clj
@@ -4,16 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.clojure/core.async "0.4.474"]
                  [org.clojure/data.json "0.2.6"]
-                 [ring "1.7.0-RC1"]
-                 [metosin/compojure-api "2.0.0-alpha18"]
                  [clj-kafka "0.3.4"]
+		 [metosin/compojure-api "2.0.0-alpha18"]
+		 [ring "1.7.0-RC1"]
                  [yogthos/config "1.1"]
-                 [clj-time "0.14.2"]]
-  :plugins [[lein-pprint "1.2.0"]]
+                 [clj-time "0.14.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
+  :plugins [[lein-pprint "1.2.0"]
+            [cider/cider-nrepl "0.17.0"]
+            [lein-ring "0.12.4"]]
+  :ring {:handler eplk.core/app
+         :nrepl {
+                 :start? true
+                 :port 6710
+                 }}
   :profiles {:prod {:resource-paths ^:replace ["config/prod"]
                     :plugins [[cider/cider-nrepl "0.16.0"]]}
              :dev  {:resource-paths ["config/dev"]

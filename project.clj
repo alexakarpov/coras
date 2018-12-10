2133fa8687e610753735f69321c82ffec610b9e9
Alexandre Karpov <alexakarpov@gmail.com>
2018-12-09 20:41:30 -0500

    don't specify nrepl version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.clojure/core.async "0.4.474"]
                  [org.clojure/data.json "0.2.6"]
@@ -14,17 +14,14 @@
                  [clj-time "0.14.2"]
                  ]
   :plugins [[lein-pprint "1.2.0"]
-            [cider/cider-nrepl "0.17.0"]
             [lein-ring "0.12.4"]]
   :ring {:handler eplk.core/app
          :nrepl {
                  :start? true
                  :port 6710
                  }}
-  :profiles {:prod {:resource-paths ^:replace ["config/prod"]
-                    :plugins [[cider/cider-nrepl "0.17.0"]]}
-             :dev  {:resource-paths ["config/dev"]
-                    :plugins [[cider/cider-nrepl "0.17.0"]]}
+  :profiles {:prod {:resource-paths ^:replace ["config/prod"]}
+             :dev  {:resource-paths ["config/dev"]}
              :uberjar {:aot :all}}
   :main ^:skip-aot eplk.core
   :target-path "target/%s")

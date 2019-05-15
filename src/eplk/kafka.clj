3e4a72e8804c52dbc4fd9fbaaed341508a8f3b85
Alexandre Karpov <alexakarpov@gmail.com>
2019-05-14 20:42:27 -0400

    trim

--- src/eplk/kafka.clj
+++ src/eplk/kafka.clj
@@ -29,45 +29,25 @@
 
   )
 
-(defn make-producer []
-  (K.out/producer {::K/nodes [[(:kafka-host env) 9002]]
-                   ::K/serializer.key (K/serializers :long)
-                   ::K/serializer.value :string
-                   ::K.out/configuration {"client.id" "clj-producer"}}))
+(defn producer-conf []
+  {::K/nodes [[(:kafka-host env) 9092]]
+   ::K/serializer.key (K/serializers :long)
+   ::K/serializer.value :string
+   ::K.out/configuration {"client.id" "my-producer"}})
 
 (defn send-message [i body]
   (println (format "%d received with %s" i body))
-  (with-open [producer (make-producer) ]
-    (K.out/send producer
-                {::K/topic (:journal-topic env)
-                 ::K/key 42
-                 ::K/value (str "bbbody")}
-                (fn callback [exception metadata]
-                  (println (format "Event %d : %s"
-                                   i
-                                   (if exception
-                                     "FAILURE"
-                                     "SUCCESS")))))))
-
-;; ########## working ###############
-
-(with-open [producer (K.out/producer {::K/nodes [[(:kafka-host env) 9092]]
-                                      ::K/serializer.key (K/serializers :long)
-                                      ::K/serializer.value :string
-                                      ::K.out/configuration {"client.id" "my-producer"}})]
-  (doseq [i (range 5)]
+  (with-open [producer (K.out/producer (producer-conf))]
     (K.out/send producer
                 {::K/topic "ak_test_events"
                  ::K/key   i
-                 ::K/value (str "message " i)}
+                 ::K/value (format "%s with id of %d" body i)}
                 (fn callback [exception metadata]
                   (println (format "Event %d : %s"
                                    i
                                    (if exception
                                      "FAILURE"
                                      "SUCCESS")))))))
-;; ###########$$$$$$$$$$$$$$$$$######
-
 
 (send-message 12 "hello")
 
